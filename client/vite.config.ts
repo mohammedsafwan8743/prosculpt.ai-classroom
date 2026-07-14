fefe2728227418e8fe29ba3ab267927c85b6bc7a
Mohammed Safwan M <mohammedsafwan.2111067@srec.ac.in>
2026-07-14 16:46:46 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: "../",
+  envDir: path.resolve(__dirname, "../"),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
